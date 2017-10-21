be0083ad5308650f4d78da03b5037228ae92f6fb
scott <scott@scottleinweber.com>
2017-10-21 15:26:39 -0400

    windows needs ./ for files in same dir evidently.

---
+++ cart.js
@@ -0,0 +1,62 @@
+// cart.js
+
+/*** Variables ***/
+// var cartItems = [];
+
+
+/*** Functions ***/
+// on button + or - click change cart
+
+// function addToCart(argument) {
+//     // when adding on page, add to cart
+// }
+
+// function updateNavCart(){
+//     // blar
+// }
+
+
+/*** Document Load ****/
+
+$(document).ready(function() {
+
+
+  console.log("This is inside the jQuery ready func");
+
+});
+
+  // // generate a random animal when the document opens
+  // var animal = JSON.parse(localStorage.getItem("savedAnimal"));
+  // var hasSavedAnimal = false;
+  // if (animal === null) {
+  //   $("#button-storage").text("Save Animal");
+  //   animal = generateRandomAnimal();
+  // } else {
+  //   $("#button-storage").text("Clear Animal");
+  //   hasSavedAnimal = true;
+  // }
+
+  // // update the page based on the animal properties
+  // $("#animal-name").text(animal.name + " the " + animal.type);
+  // $("#animal-img").attr("src", animal.image);
+
+  // // change page background to random color
+  // $("body").css("background-color", generateRandomColor());
+
+  // $("#button-storage").click(function() {
+  //   if (hasSavedAnimal) {
+  //     // clear the animal from the browser
+  //     localStorage.removeItem("savedAnimal");
+  //     // if this button was clicked, hide button and show feedback
+  //     $("#button-storage").css("display", "none");
+  //     $("#button-action-text").text("Cleared!");
+  //     $("#button-action-text").css("display", "block");
+  //   } else {
+  //     // save the animal to the browser
+  //     localStorage.setItem("savedAnimal", JSON.stringify(animal));
+  //     // if this button was clicked, hide button and show feedback
+  //     $("#button-storage").css("display", "none");
+  //     $("#button-action-text").text("Saved!");
+  //     $("#button-action-text").css("display", "block");
+  //   }
+  // });
