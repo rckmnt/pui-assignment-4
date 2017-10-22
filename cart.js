f3c8ee46c9449a8ad2ea14533df18c8903667978
scott <scottl318@gmail.com>
2017-10-22 11:27:30 -0400

    just before Table generation attempts

--- cart.js
+++ cart.js
@@ -19,25 +19,24 @@ function swapImgs(){
   var img = document.getElementById("single_bun_img");
   if (this.value == "6"){
     img.src = "./imgs/maple_apple_pecan_halfdozen.png";
-    localStorage.setItem("cartObject", this.value);
   }
   else if (this.value == "12") {
     img.src = "./imgs/maple_apple_pecan_dozen.png";
-    localStorage.setItem("cartObject", this.value);
   }
   else{
     img.src = "./imgs/maple_apple_pecan.png";
-    localStorage.setItem("cartObject", this.value);
   }
   return false;
 }
+
+
 
 function getLSCart(){
   c = localStorage.getItem("cartObject");
   console.log(c);
 }
 
-function itemsInCart(cartArray) {
+function numItemsInCart(cartArray) {
   tempTotal = 0;
   for (var i = cartArray.length - 1; i >= 0; i--) {
     tempTotal += JSON.parse(cartArray[i]["howmany"])
@@ -45,12 +44,39 @@ function itemsInCart(cartArray) {
   return tempTotal
 }
 
-JSON.parse(localStorage.cartArray)
+function updateMenuCart() {
+  if (localStorage != "undefined"){
+    $("#cart_menu_txt").text("Your Cart (" + numItemsInCart(JSON.parse(localStorage.cartArray)) + ")");
+  }
+}
 
+function getItemsFromCart() {
+  stuff = JSON.parse(localStorage.cartArray);
+  return stuff
+}
+
+// function updateCartTable(){
+//   // On Cart Page, populate Table with localStorage Values
+
+//   if (localStorage != "undefined"){
+//     for (var i = getItemsFromCart().length - 1; i >= 0; i--) {
+//       $("#cart").find('tbody').append($('<tr class="product-row"><td class="image-col"> Image </td><td class="product-col"> Product </td> <td class="edit-col"> Edit </td> <td class="quant-col"> Qty </td> <td class="subtotal-col"> subtotal </td>'));
+
+//       c = getItemsFromCart()[i];
+//       $(".img-row").attr("src", "./imgs/maple_apple_pecan_halfdozen.png")
+//       $(".product-row").text("Product name + " c.[second] + c.[third] + " if exists");
+//       $(".edit-row").text("click here to remove");
+//       $(".quant-row").text(c["howmany"]);
+//       $(".subtotal-row").text("Subtotal");
+//     }
+// }
 
 /********* Document Load ****/
 
 $(document).ready(function() {
+
+  // menu cart
+  updateMenuCart();
 
   // Swap images on dropdown change
   swapImgs();
@@ -78,11 +104,27 @@ $(document).ready(function() {
     cartArray.push(cartObject);
     localStorage.setItem("cartArray", JSON.stringify(cartArray));
 
+    // update full Cart Table
+    //updateCartTable();
+
     // Add items number to Cart(i) in Menu
-    if (localStorage != "undefined"){
-      $("#cart_menu_txt").text("Your Cart (" + JSON.parse(localStorage.cartArray).length + ")");
-    }
+    updateMenuCart();
+
   });
 });
+
+
+// JSON.parse(localStorage.cartArray).length
+
+  // add table row
+    // add image-col  if 1, 6 or 12, swap img
+    // add product-col write name of prodct - also add second and third flavors + qty
+    // add edit-col - nothing
+    // add quant-col - howmany
+    // add subtotal-col = howmany * qty * price
+
+  // On remove click
+    // table remove
+    // entire line
 
 
