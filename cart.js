6acb06e5c05341b5035c72a1e8066f58997e4644
scott <scott@scottleinweber.com>
2017-10-21 20:33:50 -0400

    cart - begin to JS

--- cart.js
+++ cart.js
@@ -1,33 +1,13 @@
-// cart.js
-
-/*** Variables ***/
-// var cartItems = [];
-
-
-/*** Functions ***/
-// on button + or - click change cart
-
-function addToCart(argument) {
-    // when adding on page, add to cart
-}
-
-function updateNavCart(){
-    // blar
-}
-
-
-//      Cart Constructor
+/********* Cart Constructor ****/
 
 var cartObject = {"bun-types": null, "bun-count":null}
-
-var cart = JSON.parse(localStorage.getItem("cartObject"));
+// var cart = JSON.parse(localStorage.getItem("cartObject"));
 var hasSavedCart = false;
 
-function newCart(){
 
-}
+/********* Basic Functions ****/
 
-// swaps product img on select dropdown change
+//swaps product img on select dropdown change
 function swapImgs(){
   var img = document.getElementById("single_bun_img");
   if (this.value == "6"){
@@ -45,66 +25,45 @@ function swapImgs(){
   return false;
 }
 
+function getLSCart(){
+  c = localStorage.getItem("cartObject");
+  console.log(c);
+}
 
 
-/*** Document Load ****/
+/********* Document Load ****/
 
 $(document).ready(function() {
+
   // Swap images on dropdown change
   swapImgs();
   document.getElementById("quantity_select").onchange = swapImgs;
   console.log("This is inside the jQuery ready func");
 
-  // get Cart from localStorage
-  if (hasSavedCart == true) {
-    var cart = JSON.parse(localStorage.getItem("cartObject"));
-  }
+  $("#cart_menu_txt").text("Cart (" + localStorage.cartObject + ")");
+
   // var hasCartObject = false;
+  getLSCart();
 
-  // if (cart === null) {
-  //   $("#button-storage").text("Save cart");
-  //   cart = generateRandomcart();
-  // } else {
-  //   $("#button-storage").text("Clear cart");
-  //   hascartObject = true;
-  // }
-
-  // Prints value of Add to Cart Button
+  // On Add to Click Button
   $("#cart_button").click(function() {
     butt = document.getElementById("quantity_select");
     if (butt.value == 13) {
       console.log("Value =" + butt.value);
-      localStorage.setItem("cartObject"{bun-count}, butt.value);
+      localStorage.setItem("cartObject", butt.value);
     } else if (butt.value == 6) {
       console.log("Value =" + butt.value);
+      localStorage.setItem("cartObject", butt.value);
     } else{
       console.log("Value =" + butt.value);
+      localStorage.setItem("cartObject", butt.value);
     };
+
+    // Add items number to Cart(i) in Menu
+    if (localStorage != "undefined"){
+      $("#cart_menu_txt").text("Your Cart (" + localStorage.cartObject + ")");
+    }
   });
 });
-
-  // // update the page based on the animal properties
-  // $("#animal-name").text(animal.name + " the " + animal.type);
-  // $("#animal-img").attr("src", animal.image);
-
-
-  // $("#button-storage").click(function() {
-  //   if (hasSavedAnimal) {
-  //     // clear the animal from the browser
-  //     localStorage.removeItem("savedAnimal");
-  //     // if this button was clicked, hide button and show feedback
-  //     $("#button-storage").css("display", "none");
-  //     $("#button-action-text").text("Cleared!");
-  //     $("#button-action-text").css("display", "block");
-  //   } else {
-  //     // save the animal to the browser
-  //     localStorage.setItem("savedAnimal", JSON.stringify(animal));
-  //     // if this button was clicked, hide button and show feedback
-  //     $("#button-storage").css("display", "none");
-  //     $("#button-action-text").text("Saved!");
-  //     $("#button-action-text").css("display", "block");
-  //   }
-  // });
-
 
 
