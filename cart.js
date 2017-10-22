2db9e8f3f44bacdd08d9a1e58b8e8b214e82ac6b
scott <scottl318@gmail.com>
2017-10-22 12:49:40 -0400

    an hour looking for a curly brace

--- cart.js
+++ cart.js
@@ -7,15 +7,14 @@ var cartObject = {
   "third": null,
 }
 
-// var cart = JSON.parse(localStorage.getItem("cartObject"));
-var hasSavedCart = false;
-
 var cartArray = [];
+
 
 /********* Basic Functions ****/
 
-//swaps product img on select dropdown change
+
 function swapImgs(){
+  //swaps product img on select dropdown change
   var img = document.getElementById("single_bun_img");
   if (this.value == "6"){
     img.src = "./imgs/maple_apple_pecan_halfdozen.png";
@@ -29,11 +28,10 @@ function swapImgs(){
   return false;
 }
 
-
-
 function getLSCart(){
-  c = localStorage.getItem("cartObject");
-  console.log(c);
+  // parses localStorage cartArray into object
+  c = JSON.parse(localStorage.getItem("cartArray"));
+  return c;
 }
 
 function numItemsInCart(cartArray) {
@@ -41,35 +39,56 @@ function numItemsInCart(cartArray) {
   for (var i = cartArray.length - 1; i >= 0; i--) {
     tempTotal += JSON.parse(cartArray[i]["howmany"])
   }
-  return tempTotal
+  return tempTotal;
 }
 
 function updateMenuCart() {
   if (localStorage != "undefined"){
     $("#cart_menu_txt").text("Your Cart (" + numItemsInCart(JSON.parse(localStorage.cartArray)) + ")");
   }
+  return false;
 }
 
 function getItemsFromCart() {
   stuff = JSON.parse(localStorage.cartArray);
-  return stuff
+  return stuff;
 }
 
-// function updateCartTable(){
-//   // On Cart Page, populate Table with localStorage Values
+function cartTotal(){
+  var subtotal = 0;
+  if (localStorage != "undefined"){
+      for (var i = getItemsFromCart().length - 1; i >= 0; i--){
+        subtotal += (3 * getItemsFromCart()[i]["howmany"] * getItemsFromCart()[i]["qty"]);
+      }
+    }
+  return subtotal;
+}
 
-//   if (localStorage != "undefined"){
-//     for (var i = getItemsFromCart().length - 1; i >= 0; i--) {
-//       $("#cart").find('tbody').append($('<tr class="product-row"><td class="image-col"> Image </td><td class="product-col"> Product </td> <td class="edit-col"> Edit </td> <td class="quant-col"> Qty </td> <td class="subtotal-col"> subtotal </td>'));
 
-//       c = getItemsFromCart()[i];
-//       $(".img-row").attr("src", "./imgs/maple_apple_pecan_halfdozen.png")
-//       $(".product-row").text("Product name + " c.[second] + c.[third] + " if exists");
-//       $(".edit-row").text("click here to remove");
-//       $(".quant-row").text(c["howmany"]);
-//       $(".subtotal-row").text("Subtotal");
-//     }
-// }
+//On Cart Page, populate Table with localStorage Values
+function updateCartTable(){
+  console.log('stupid');
+  // if (localStorage != "undefined"){
+  //         for (var i = getItemsFromCart().length - 1; i >= 0; i--) {
+
+  //           cartObject = getItemsFromCart()[i];
+
+  //           var howmany = cartObject["howmany"];
+  //           var qty = cartObject["qty"];
+  //           var second = cartObject["second"];
+  //           var third = cartObject["third"];
+  //           //$("#cart").find('tbody').append($('<tr class="product-row"><td class="image-col"> Image </td><td class="product-col"> Product </td> <td class="edit-col"> Edit </td> <td class="quant-col"> Qty </td> <td class="subtotal-col"> subtotal </td>'));
+  //           // $(".img-row").attr("src", "./imgs/maple_apple_pecan_halfdozen.png");
+  //           $(".product-row").text("Product name + " + second + third + " if exists");
+  //           $(".edit-row").text("click here to remove");
+  //           $(".quant-row").text(howmany);
+  //           $(".subtotal-row").text("Subtotal");
+  //         }
+  //   }
+  // return getItemsFromCart();
+}
+
+
 
 /********* Document Load ****/
 
@@ -78,12 +97,11 @@ $(document).ready(function() {
   // menu cart
   updateMenuCart();
 
-  // Swap images on dropdown change
-  swapImgs();
-  document.getElementById("quantity_select").onchange = swapImgs;
+  // print CartArray object from localStorage
+  console.log(getLSCart());
 
-  // var hasCartObject = false;
-  getLSCart();
+  // update full Cart Table
+  // updateCartTable();
 
   // On Add to Click Button
   $("#cart_button").click(function() {
@@ -104,14 +122,18 @@ $(document).ready(function() {
     cartArray.push(cartObject);
     localStorage.setItem("cartArray", JSON.stringify(cartArray));
 
-    // update full Cart Table
-    //updateCartTable();
+    // Swap images on dropdown change
+    swapImgs();
+    document.getElementById("quantity_select").onchange = swapImgs;
+
 
     // Add items number to Cart(i) in Menu
     updateMenuCart();
 
   });
 });
+
+
 
 
 // JSON.parse(localStorage.cartArray).length
